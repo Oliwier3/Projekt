97693d80917bf84694ef5753961fc2a195725148
piasek <piaskowski3456@gmail.com>
2022-11-21 15:20:03 +0100

    8 if lepiej

--- app/src/main/java/com/example/projekt/MainActivity.kt
+++ app/src/main/java/com/example/projekt/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var i=0
+        var i=1
         findViewById<Button>(R.id.dalej).setOnClickListener {
             i += 1
             if (i == 1)
@@ -18,21 +18,22 @@ class MainActivity : AppCompatActivity() {
                 findViewById<ImageView>(R.id.obraz).setImageResource(R.drawable.sid2)
             if (i == 3)
                 findViewById<ImageView>(R.id.obraz).setImageResource(R.drawable.sid3)
-            if(i==4 || i==0){
+            if(i==4){
                 i=1
                 findViewById<ImageView>(R.id.obraz).setImageResource(R.drawable.sid1)
             }
         }
         findViewById<Button>(R.id.wstecz).setOnClickListener {
             i -= 1
-            if (i == 1)
+            if (i == 1) {
                 findViewById<ImageView>(R.id.obraz).setImageResource(R.drawable.sid1)
+            }
             if (i == 2)
                 findViewById<ImageView>(R.id.obraz).setImageResource(R.drawable.sid2)
             if (i == 3)
                 findViewById<ImageView>(R.id.obraz).setImageResource(R.drawable.sid3)
-            if(i==4 || i==0) {
-                i = 3
+            if(i==0){
+                i=3
                 findViewById<ImageView>(R.id.obraz).setImageResource(R.drawable.sid3)
             }
         }
